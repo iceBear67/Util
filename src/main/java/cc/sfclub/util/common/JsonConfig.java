f1f9c5c0796613b607f0fc2a51c16112df536c89
iceBear67 <icebear67@sfclub.cc>
2020-06-14 15:10:09 +0800

    Improved JsonConfig

--- src/main/java/cc/sfclub/util/common/JsonConfig.java
+++ src/main/java/cc/sfclub/util/common/JsonConfig.java
@@ -1,13 +1,10 @@
 package cc.sfclub.util.common;
 
-import com.google.gson.Gson;
+import cc.sfclub.util.Util;
 import lombok.SneakyThrows;
-
-import java.io.*;
 
 public class JsonConfig {
     private transient String root;
-    private static transient Gson gson=new Gson();
     public JsonConfig(String rootDir) {
         root = rootDir;
     }
@@ -21,12 +18,7 @@ public class JsonConfig {
      */
     @SneakyThrows
     public void saveConfig() {
-            byte[] bWrite = gson.toJson(this).getBytes();
-            File conf = new File(root + "/" + getConfigName());
-            BufferedOutputStream os = new BufferedOutputStream(new FileOutputStream(conf));
-            os.write(bWrite);
-            os.flush();
-            os.close();
+        SimpleFile.write(root + "/" + getConfigName(), Util.getGson().toJson(this));
     }
 
     /**
@@ -34,14 +26,9 @@ public class JsonConfig {
      */
     @SneakyThrows
     public JsonConfig reloadConfig() {
-            BufferedInputStream f = new BufferedInputStream(new FileInputStream(root + "/" + getConfigName()));
-            int size = f.available();
-            StringBuilder confText = new StringBuilder();
-            for (int i = 0; i < size; i++) {
-                confText.append((char) f.read());
-            }
-            JsonConfig pluginConfig = gson.fromJson(confText.toString(), this.getClass());
-            pluginConfig.root = root;
-            return pluginConfig;
+        String confText = SimpleFile.readAsString(root + "/" + getConfigName());
+        JsonConfig pluginConfig = Util.getGson().fromJson(confText, this.getClass());
+        pluginConfig.root = root;
+        return pluginConfig;
     }
 }
