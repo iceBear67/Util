b0b7a673302717a13c9ebcb1dc9f523c7513b961
iceBear67 <icebear67@sfclub.cc>
2020-06-16 16:32:35 +0800

    Improve since and until

--- src/main/java/cc/sfclub/util/Since.java
+++ src/main/java/cc/sfclub/util/Since.java
@@ -3,7 +3,7 @@ package cc.sfclub.util;
 import java.lang.annotation.*;
 
 @Documented
-@Target({ElementType.METHOD, ElementType.TYPE})
+@Target({ElementType.METHOD, ElementType.TYPE, ElementType.FIELD})
 @Retention(RetentionPolicy.SOURCE)
 public @interface Since {
     String value();
